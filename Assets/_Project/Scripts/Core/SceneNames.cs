a479d21118fbec876f8546fcdbb0c240a505ad27
Syed Bilal S. <bilal.quecko@gmail.com>
2026-08-27 18:44:57 +0500

    feat: add scene name constants

---
+++ Assets/_Project/Scripts/Core/SceneNames.cs
@@ -0,0 +1,9 @@
+namespace MeraBrand.Expo.Core
+{
+    public static class SceneNames
+    {
+        public const string Boot = "00_Boot";
+        public const string MainMenu = "01_MainMenu";
+        public const string Exhibition = "02_Exhibition";
+    }
+}
