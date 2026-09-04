946ff44360acf9e5629cb78e4b1975f1c8769aa5
Syed Bilal S. <bilal.quecko@gmail.com>
2026-09-04 05:22:34 -0500

    mob system

---
+++ Assets/UTS_PRO/Scripts/Car/CarControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControl : MonoBehaviour 
+{
+//	private CarMove m_Car;
+
+//	private void Awake()
+//	{
+//		m_Car = GetComponent<CarMove>();
+//	}
+
+
+//	private void FixedUpdate()
+//	{
+//		// pass the input to the car!
+//		float h = Input.GetAxis("Horizontal");
+//		float v = Input.GetAxis("Vertical");
+//#if !MOBILE_INPUT
+//		float handbrake = Input.GetAxis("Jump");
+//		m_Car.Move(v, v, handbrake, h);
+//#else
+//		m_Car.Move(v, v, 0f, h);
+//#endif
+//	}
+}
